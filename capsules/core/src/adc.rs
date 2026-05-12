1920b12238506a461bede1f3317a2f1ca44a890d
Brad Campbell <bradjc5@gmail.com>
2026-05-12 10:54:18 -0400

    capsules: clippy

--- capsules/core/src/adc.rs
+++ capsules/core/src/adc.rs
@@ -367,7 +367,7 @@ impl<'a, A: hil::adc::Adc<'a> + hil::adc::AdcHighSpeed<'a>> AdcDedicated<'a, A> 
                     app.app_buf_offset.set(0);
                     self.channel.set(channel);
                     // start a continuous sample
-                    let res = self.adc_buf1.take().map_or(Err(ErrorCode::BUSY), |buf1| {
+                    self.adc_buf1.take().map_or(Err(ErrorCode::BUSY), |buf1| {
                         self.adc_buf2
                             .take()
                             .map_or(Err(ErrorCode::BUSY), move |buf2| {
@@ -402,8 +402,7 @@ impl<'a, A: hil::adc::Adc<'a> + hil::adc::AdcHighSpeed<'a>> AdcDedicated<'a, A> 
                                         |()| Ok(()),
                                     )
                             })
-                    });
-                    res
+                    })
                 })
                 .map_err(|err| {
                     if err == kernel::process::Error::NoSuchApp
